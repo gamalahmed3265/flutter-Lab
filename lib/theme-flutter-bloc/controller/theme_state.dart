73e2ef85a7e429db6b78a3222af114bc1489e671
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-19 13:37:25 +0300

    add theme

---
+++ lib/theme-flutter-bloc/controller/theme_state.dart
@@ -0,0 +1,10 @@
+part of 'theme_cubit.dart';
+
+@immutable
+abstract class ThemeState {}
+
+// initlize app
+class ThemeInit extends ThemeState {}
+
+//will controller theme of app
+class ThemeChange extends ThemeState {}
