6736f0ab1ce35977ba33ad43cd3558408e70f038
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2023-08-01 14:28:04 +0300

    what difference between cubit and bloc

---
+++ lib/presentation/controller/counter_cubit/cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(const CounterStateInitial());
+
+  void increment() => emit(CounterStateData(state.counter + 1));
+  void decrement() => emit(CounterStateData(state.counter - 1));
+}
---
+++ lib/presentation/controller/counter_cubit/state.dart
@@ -0,0 +1,17 @@
+part of 'counter_cubit.dart';
+
+abstract class CounterState extends Equatable {
+  final int counter;
+  const CounterState(this.counter);
+
+  @override
+  List<Object> get props => [counter];
+}
+
+class CounterStateInitial extends CounterState {
+  const CounterStateInitial() : super(0);
+}
+
+class CounterStateData extends CounterState {
+  const CounterStateData(int counter) : super(counter);
+}
